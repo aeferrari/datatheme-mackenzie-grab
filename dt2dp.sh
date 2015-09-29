bdee64d95b8a8347953d9151503d102c726731c5
Boris Cosic <boris.cosic@tesera.com>
2015-09-29 17:16:22 -0600

    Schema fixes.

--- dt2dp.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo '{"name": "mackenzie-grab-dataset",'
-echo '"datatheme": { "name": "mackenzie.grab", "version": "1.0.1", "repository": "https://github.com/tesera/datatheme-mackenzie-grab.git"},'
-echo '"resources": ['
-    cat ./schemas/grab_data.json
-    echo ,
-    cat ./schemas/grab_meta.json
-echo ']}'
